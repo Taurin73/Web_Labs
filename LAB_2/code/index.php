e92371c34a30f302509728c96d7cfe949e438bee
Taurin73 <113453931+Taurin73@users.noreply.github.com>
2024-10-17 01:47:29 +0200

    Added numeral variables and printed calculations

--- LAB_2/code/index.php
+++ LAB_2/code/index.php
@@ -7,3 +7,14 @@
 // Task 1
 $order = &$very_bad_unclear_name;
 $order .= ", two number 9s, a number 9 large, a number 6 extra dip and a number 7";
+
+// Task 2
+$toPay = 130;
+echo "The price is $$toPay";
+echo "\n";
+
+$taxes = 3.5;
+echo "Tax: 3.5%\n"; // No need to use echo for \n
+
+echo 5+6+1;
+echo "\n";
